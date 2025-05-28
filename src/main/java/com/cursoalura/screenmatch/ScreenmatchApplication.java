72dea1a57625088e1cc662f30253edac71b6de62
Vania Santillan Aguilar <142811154+VaniaSantillan@users.noreply.github.com>
2025-05-28 11:16:43 -0600

    Primer avance de la plataforma.
    En esta primera parte del proyecto solo se han usado lambdas, Streams y Spring  Framework

---
+++ src/main/java/com/cursoalura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package com.cursoalura.screenmatch;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.cursoalura.screenmatch.Principal.Principal;
+
+
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+    @Override
+    public void run(String... args) throws Exception {
+        Principal principal = new Principal();
+        principal.muestraElMenu();
+    }
+
+}
---
+++ src/main/java/com/cursoalura/screenmatch/model/DatosSerie.java
@@ -0,0 +1,12 @@
+package com.cursoalura.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DatosSerie(
+    @JsonAlias ("Title") String titulo,
+    @JsonAlias ("totalSeasons") Integer totalDeTemporadas,
+    @JsonAlias ("imdbRating") String evaluacion) {
+
+
+
+}
